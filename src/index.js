4a7477613a4a72502b34ca9fe96764bfff280c5b
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-08 17:01:47 -0500

    adding recycling pipes

--- src/index.js
+++ src/index.js
@@ -31,9 +31,12 @@ const VELOCITY = 200;
 const PIPES_TO_RENDER = 4;
 const flapVelocity = 250;
 let bird = null;
+let pipes = null;
+
 let pipeHorizontalDistance = 0;
 
 const pipeVerticalDistanceRange = [150, 250];
+const pipeHorizontalDistanceRange = [300, 400];
 
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
 
@@ -43,11 +46,16 @@ function create() {
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, "bird").setOrigin(0);
   bird.body.gravity.y = 400;
 
+  pipes = this.physics.add.group();
+
   for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    const upperPipe = this.physics.add.sprite(0, 0, "pipe").setOrigin(0, 1);
-    const lowerPipe = this.physics.add.sprite(0, 0, "  e").setOrigin(0, 0);
+    const upperPipe = pipes.create(0, 0, "pipe").setOrigin(0, 1);
+    const lowerPipe = pipes.create(0, 0, "pipe").setOrigin(0, 0);
+
     placePipe(upperPipe, lowerPipe);
   }
+
+  pipes.setVelocityX(-200);
 
   this.input.on("pointerdown", flap);
 
@@ -59,21 +67,42 @@ function update(time, delta) {
     // alert("you have lost");
     restartBirdPositio();
   }
+  recyclePipes();
 }
 
 function placePipe(uPipe, lPipe) {
-  pipeHorizontalDistance += 400;
-  let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
-  let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+  const rightMostX = getRightMostPipe();
+  const pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+  const pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+  const pipeHorizontalDistance = Phaser.Math.Between(...pipeHorizontalDistanceRange);
 
-  uPipe.x = pipeHorizontalDistance;
+  uPipe.x = rightMostX + pipeHorizontalDistance;
   uPipe.y = pipeVerticalPosition;
 
   lPipe.x = uPipe.x;
   lPipe.y = uPipe.y + pipeVerticalDistance;
+}
 
-  lPipe.body.velocity.x = -200;
-  uPipe.body.velocity.x = -200;
+function recyclePipes() {
+  const tempPipes = [];
+  pipes.getChildren().forEach((pipe) => {
+    if (pipe.getBounds().right <= 0) {
+      tempPipes.push(pipe);
+      if (tempPipes.length === 2) {
+        placePipe(...tempPipes);
+      }
+    }
+  });
+}
+
+function getRightMostPipe() {
+  let rightMostX = 0;
+
+  pipes.getChildren().forEach(function (pipe) {
+    rightMostX = Math.max(pipe.x, rightMostX);
+  });
+
+  return rightMostX;
 }
 
 function restartBirdPositio() {
