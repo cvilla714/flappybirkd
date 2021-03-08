599cc976c985ce793167606bef03115709e68eae
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-08 16:16:07 -0500

    renderging multiple piples

--- src/index.js
+++ src/index.js
@@ -28,13 +28,14 @@ function preload() {
 }
 
 const VELOCITY = 200;
+const PIPES_TO_RENDER = 4;
 const flapVelocity = 250;
 let bird = null;
 let upperPipe = null;
 let lowerPipe = null;
+let pipeHorizontalDistance = 0;
+
 const pipeVerticalDistanceRange = [150, 250];
-let pipeVeritcaldistance = Phaser.Math.Between(pipeVerticalDistanceRange[0], pipeVerticalDistanceRange[1]);
-let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVeritcaldistance);
 
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
 
@@ -43,17 +44,20 @@ function create() {
   // bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, "bird").setOrigin(0);
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, "bird").setOrigin(0);
   bird.body.gravity.y = 400;
-  upperPipe = this.physics.add.sprite(400, pipeVerticalPosition, "pipe").setOrigin(0, 1);
-  lowerPipe = this.physics.add.sprite(400, upperPipe.y + pipeVeritcaldistance, "pipe").setOrigin(0, 0);
-  // this.input.on("pointerdown", function () {
-  // console.log("pressing mouse");
-  // });
+
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    pipeHorizontalDistance += 400;
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+    let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
+
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, "pipe").setOrigin(0, 1);
+    lowerPipe = this.physics.add.sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, "pipe").setOrigin(0, 0);
+
+    upperPipe.body.velocity.x = -200;
+    lowerPipe.body.velocity.x = -200;
+  }
 
   this.input.on("pointerdown", flap);
-  // debugger;
-  // this.input.keyboard.on("keydown-SPACE", function () {
-  // console.log(`pressing the spacebar  button`);
-  // });
 
   this.input.keyboard.on("keydown-SPACE", flap);
 }
