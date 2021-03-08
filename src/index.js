0fcd6ffb7b93747003bb142f0a9deedaaa1c8714
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-08 13:17:41 -0500

    adding preload and create functions

--- src/index.js
+++ src/index.js
@@ -1,28 +1,43 @@
-
 import Phaser from "phaser";
+import sky from "./images/sky.png";
+import pajaro from "./images/bird.png";
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      gravity: { y: 200 }
-    }
+      gravity: { y: 200 },
+    },
   },
   scene: {
     preload: preload,
-    create: create
-  }
+    create: create,
+    update,
+  },
 };
 
+function preload() {
+  this.load.image("sky", sky);
+  this.load.image("bird", pajaro);
+}
+
+let bird = null;
+
+function create() {
+  this.add.image(0, 0, "sky").setOrigin(0);
+  // this.add.sprite(config.width / 2, config.height / 2, "bird").setOrigin(0);
+  // this will move the bird  1/10th of the width
+  // this.add.sprite(config.width * 0.1, config.height / 2, "bird").setOrigin(0);
+  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, "bird").setOrigin(0);
+  bird.body.gravity.y = 200;
+  // console.log(bird.body);
+  // debugger;
+}
+
+function update(time, delta) {
+  console.log(bird.body.velocity.y);
+}
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
