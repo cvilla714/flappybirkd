42626eff2f4d5d2364552f198902226ca1e19eb9
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-08 15:09:59 -0500

    added the function to restart the player position

--- src/index.js
+++ src/index.js
@@ -26,19 +26,21 @@ function preload() {
 }
 
 const VELOCITY = 200;
-let flapVelocity = 250;
+const flapVelocity = 250;
 let bird = null;
+const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
 
 function create() {
   this.add.image(0, 0, "sky").setOrigin(0);
-  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, "bird").setOrigin(0);
+  // bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, "bird").setOrigin(0);
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, "bird").setOrigin(0);
 
   // this.input.on("pointerdown", function () {
   // console.log("pressing mouse");
   // });
 
   this.input.on("pointerdown", flap);
-
+  debugger;
   // this.input.keyboard.on("keydown-SPACE", function () {
   // console.log(`pressing the spacebar  button`);
   // });
@@ -46,8 +48,18 @@ function create() {
   this.input.keyboard.on("keydown-SPACE", flap);
 }
 
-function update(time, delta) {}
+function update(time, delta) {
+  if (bird.body.position.y > config.height || bird.body.position.y < -bird.height) {
+    // alert("you have lost");
+    restartBirdPositio();
+  }
+}
 
+function restartBirdPositio() {
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0;
+}
 function flap() {
   bird.body.velocity.y = -flapVelocity;
 }
