b4f68a9403622eae08d81904cc0782e0d9dbdfb6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-09 00:13:47 -0500

    keep the best score

--- src/playScene.js
+++ src/playScene.js
@@ -69,7 +69,9 @@ class PlayScene extends Phaser.Scene {
 
   createScore() {
     this.score = 0;
+    const bestScore = localStorage.getItem("bestScore");
     this.scoreText = this.add.text(16, 16, `Score: ${0}`, { fontSize: "32px", fill: "#000" });
+    this.add.text(16, 52, `Best score: ${bestScore || 0}`, { fontSize: "18px", fill: "#000" });
   }
 
   handleInputs() {
@@ -105,6 +107,7 @@ class PlayScene extends Phaser.Scene {
         if (tempPipes.length === 2) {
           this.placePipe(...tempPipes);
           this.increaseScore();
+          this.saveBestScore();
         }
       }
     });
@@ -120,10 +123,21 @@ class PlayScene extends Phaser.Scene {
     return rightMostX;
   }
 
+  saveBestScore() {
+    const bestScoreText = localStorage.getItem("bestScore");
+    const bestScore = bestScoreText && parseInt(bestScoreText, 10);
+
+    if (!bestScore || this.score > bestScore) {
+      localStorage.setItem("bestScore", this.score);
+    }
+  }
+
   //   restartBirdPosition() {
   gameOver() {
     this.physics.pause();
     this.bird.setTint(0xee4824);
+
+    this.saveBestScore();
 
     this.time.addEvent({
       delay: 1000,
