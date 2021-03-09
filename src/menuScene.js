073d14a447c606a9d2ec11278f631239a031606d
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-09 09:18:34 -0500

    created the menu scene

---
+++ src/menuScene.js
@@ -0,0 +1,19 @@
+import Phaser from "phaser";
+import sky from "./images/sky.png";
+
+class MenuScene extends Phaser.Scene {
+  constructor(config) {
+    super("MenuScene");
+    this.config = config;
+  }
+
+  preload() {
+    this.load.image("sky", sky);
+  }
+
+  create() {
+    this.add.image(0, 0, "sky").setOrigin(0);
+  }
+}
+
+export default MenuScene;
